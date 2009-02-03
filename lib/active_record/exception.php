839de910385963cd0f9c471054076e4ce4415ed9
julien <julien@omoikane.(none)>
2009-02-03 17:14:52 +0100

    Added some more empty methods to ActiveRecord::Base.

---
+++ lib/active_record/exception.php
@@ -0,0 +1,11 @@
+<?php
+
+define('ActiveRecord_Exception_SaveFailed',   'SaveFailed');
+define('ActiveRecord_Exception_CreateFailed', 'CreateFailed');
+
+class ActiveRecord_Exception extends MisagoException
+{
+
+}
+
+?>
