392feead6f2661a819d2a3f4977cd922f4bc7ddd
Julien <ysbaddaden@gmail.com>
2009-02-11 02:53:48 +0100

    Implemented ActiveRecord::create() and ActiveRecord::update().

--- lib/active_record/base.php
+++ lib/active_record/base.php
@@ -11,6 +11,7 @@ class ActiveRecord_Base extends ActiveRecord_Record
   protected $primary_key = 'id';
   protected $columns     = array();
   
+  
   # TODO: Post(:id)
   function __construct($arg=null)
   {
@@ -47,13 +48,78 @@ class ActiveRecord_Base extends ActiveRecord_Record
     return parent::__set($attr, $value);
   }
   
-  function create()
+  /**
+   * Creates a new record (saved in database).
+   * 
+   * <code>
+   * $user  = $user->create(array('name' => 'John'));
+   * $users = $user->create(array('name' => 'Jane'), array('name' => 'Billy'));
+   * </code>
+   */
+  function create(array $attributes)
+  {
+    if (func_num_args() > 1)
+    {
+      $args    = func_get_args();
+      $records = array();
+      
+      foreach($args as $attributes) {
+        $records[] = $this->create($attributes);
+      }
+      return $records;
+    }
+    else
+    {
+      $class  = get_class($this);
+      $record = new $class($attributes);
+      
+      if ($record->_create()) {
+        return $record;
+      }
+    }
+    return false;
+  }
+  
+  /**
+   * Updates one or many records.
+   * 
+   * <code>
+   * # update one row
+   * $user = $user->update(1, array('name' => 'Joe'));
+   *
+   * # update many rows
+   * $people = array(1 => array('name' => 'Polly'), 1 => array('name' => 'Jean'));
+   * $users = $user->update(array_keys($people), array_values($people));
+   * </code>
+   * 
+   * FIXME: Must return ActiveRecord objects!
+   * FIXME: Record must be loaded before it is updated, and only changed attributes must be recorded.
+   */
+  function update($id, $attributes)
+  {
+    if (is_array($id))
+    {
+      $records = array();
+      $i = 0;
+      foreach($id as $_id)
+      {
+        $records[$i] = $this->update($_id, $attributes[$i]);
+        $i++;
+      }
+      return $records;
+    }
+    else
+    {
+      $conditions = array($this->primary_key => $id);
+      return $this->db->update($this->table_name, $attributes, $conditions);
+    }
+  }
+  
+  private function _create()
   {
     $id = $this->db->insert($this->table_name, $this->__attributes, $this->primary_key);
-    if ($id)
-    {
-      $this->{$this->primary_key} = $id;
-      return true;
+    if ($id) {
+      return $this->{$this->primary_key} = $id;
     }
     return false;
   }
--- test/unit/active_record/test_base.php
+++ test/unit/active_record/test_base.php
@@ -49,14 +49,53 @@ class Test_ActiveRecord_Base extends Unit_Test
   function test_create()
   {
     $product = new Product();
-    $product->name  = 'azerty';
-    $product->price = 9.95;
-    $test = $product->create();
+    $product = $product->create(array('name' => 'azerty', 'price' => 9.95));
     
-    $this->assert_true("Must have created product 'azerty'", $test);
-    $this->assert_true("Must have set product.id", isset($product->id));
-    $this->assert_type("product.id must be an integer (typecast)", $product->id, 'integer');
+    $this->assert_equal('Created product must be a Product', get_class($product), 'Product');
+    $this->assert_equal("", $product->name, 'azerty');
+    $this->assert_equal("", $product->price, 9.95);
+    $this->assert_type("product.id must be an integer", $product->id, 'integer');
   }
+  
+  function test_create_many()
+  {
+    $data1 = array('name' => "qwerty", 'price' =>  5.98);
+    $data2 = array('name' => "bepo",   'price' => 10.55);
+  
+    $product = new Product();
+    $products = $product->create($data1, $data2);
+    
+    $this->assert_equal('Created product must be a Product', get_class($products[0]), 'Product');
+    $this->assert_equal("", $products[1]->name, 'bepo');
+    $this->assert_equal("", $products[0]->price, 5.98);
+  }
+  
+  function test_find()
+  {
+    
+  }
+  
+  /*
+  function test_update()
+  {
+    $product = new Product();
+    $product = $product->update(1, array('name' => 'swerty'));
+    
+    $this->assert_equal('must be a Product', get_class($product), 'Product');
+    $this->assert_equal("attribute must have changed", $product->name, 'swerty');
+  }
+  
+  function test_update_many()
+  {
+    $product = new Product();
+    $products = $product->update(array(1, 2), array(array('name' => 'swerty'), array('name' => 'bepo')));
+    
+    $this->assert_equal('must be an array of products', get_class($products[0]), 'Product');
+    $this->assert_equal("", $products[1]->name, 'bepo');
+    $this->assert_equal("", $products[0]->name, 'swerty');
+  }
+  */
+  
 }
 
 new Test_ActiveRecord_Base();
