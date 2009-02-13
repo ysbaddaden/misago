4c5f63172fc7c22b3166cfa3f534e7d3b6728df7
Julien <ysbaddaden@gmail.com>
2009-02-13 19:41:31 +0100

    Implemented emulation functions for APC data caching (for a single request only).

---
+++ lib/fake_apc.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Emulates APC caching features when APC is missing.
+ * 
+ * TODO: Save data on disk, to emulate between requests cache.
+ * TODO: Check for data expiration (done once at request startup?).
+ */
+
+$GLOBALS['__fake_apc_data'] = array();
+#$__fake_apc_expires = array();
+
+/**
+ * Caches a variable in the data store, only if it's not already stored. 
+ */
+function apc_add($key, $var, $ttl=0)
+{
+  if (!array_key_exists($key, $GLOBALS['__fake_apc_data'])) {
+    return apc_store($key, $var, $ttl);
+  }
+  return true;
+}
+
+/**
+ * Cache a variable in the data store. 
+ */
+function apc_store($key, $var, $ttl=0)
+{
+  $GLOBALS['__fake_apc_data'][$key] = $var;
+#  $__fake_apc_expires[$key] = $ttl ? time() + $ttl : strtotime('+1 year');
+  return true;
+}
+
+/**
+ * Fetchs a stored variable from the cache.
+ */
+function apc_fetch($key, &$success=null)
+{
+  if (isset($GLOBALS['__fake_apc_data'][$key]))
+  {
+    $success = true;
+    return $GLOBALS['__fake_apc_data'][$key];
+  }
+  return $success = false;
+}
+
+/**
+ * Removes a stored variable from the cache.
+ */
+function apc_delete($key)
+{
+  unset($GLOBALS['__fake_apc_data'][$key]);
+#  unset($__fake_apc_expires[$key]);
+  return true;
+}
+
+function apc_clear_cache()
+{
+  $GLOBALS['__fake_apc_data']    = array();
+#  $__fake_apc_expires = array();
+  return true;
+}
+
+?>
---
+++ test/unit/test_fake_apc.php
@@ -0,0 +1,43 @@
+<?php
+
+$location = dirname(__FILE__).'/../../';
+$_ENV['MISAGO_ENV'] = 'test';
+
+require_once "$location/lib/unit_test.php";
+require_once "$location/lib/fake_apc.php";
+
+class TestFakeApc extends Unit_Test
+{
+  function test_apc_store()
+  {
+    $this->assert_true("", apc_store('foo', 'BAR'));
+    $this->assert_equal("", apc_fetch('foo'), 'BAR');
+    
+    apc_store('foo', 'FOO');
+    $this->assert_equal("", apc_fetch('foo'), 'FOO');
+  }
+  
+  function test_apc_add()
+  {
+    apc_add('foofoo', 'BAR');
+    apc_add('foofoo', 'FOO');
+    $this->assert_equal("", apc_fetch('foofoo'), 'BAR');
+  }
+  
+  function test_apc_delete()
+  {
+    apc_delete('foofoo');
+    $this->assert_false("", apc_fetch('foofoo'));
+  }
+  
+  function apc_clear_cache()
+  {
+    $this->assert_equal("", apc_fetch('foo'), 'bar');
+    apc_clear_cache();
+    $this->assert_false("", apc_fetch('foo'));
+  }
+}
+
+new TestFakeApc();
+
+?>
