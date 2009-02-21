42d742c5fdfe82f8621e6df95a743f0e372199a4
Julien <ysbaddaden@gmail.com>
2009-02-22 00:36:51 +0100

    Added models to test_app in order to test ActiveRecord associations.

---
+++ test/test_app/test/unit/test_basket.php
@@ -0,0 +1,17 @@
+<?php
+
+$location = dirname(__FILE__).'/../..';
+require_once "$location/lib/misago/lib/unit_test.php";
+require_once "$location/config/boot.php";
+
+class Test_Basket extends Unit_Test
+{
+  function test_true()
+  {
+    $this->assert_true('true', true);
+  }
+}
+
+new Test_Basket();
+
+?>
