4baa49cbf82441aba276fff4f6c704cdcc78652d
Julien <ysbaddaden@gmail.com>
2009-02-25 01:44:15 +0100

    Refactored associations in ActiveRecord::Base.

--- lib/active_record/base.php
+++ lib/active_record/base.php
@@ -19,9 +19,14 @@ class ActiveRecord_Base extends ActiveRecord_Record
   
   protected $new_record  = true;
   
-  protected $belongs_to = array();
-  protected $has_one    = array();
-  protected $has_many   = array();
+  protected $associations = array(
+    'belongs_to' => array(),
+    'has_one'    => array(),
+    'has_many'   => array(),
+  );
+  protected $belongs_to   = array();
+  protected $has_one      = array();
+  protected $has_many     = array();
   
   
   function __construct($arg=null)
@@ -38,6 +43,12 @@ class ActiveRecord_Base extends ActiveRecord_Record
       $this->columns = $this->db->columns($this->table_name);
       apc_store($apc_key, $this->columns);
     }
+    
+    # relationships
+    # OPTIMIZE: Cache relationships configuration in APC (?)
+    $this->configure_associations('belongs_to');
+    $this->configure_associations('has_one');
+    $this->configure_associations('has_many');
     
     # args
     if ($arg !== null)
@@ -58,6 +69,47 @@ class ActiveRecord_Base extends ActiveRecord_Record
   {
     foreach($arg as $attribute => $value) {
       $this->$attribute = $value;
+    }
+  }
+  
+  private function configure_associations($type)
+  {
+    foreach($this->$type as $i => $assoc)
+    {
+      if (is_integer($i))
+      {
+        $name = $assoc;
+        unset($this->$type[$i]);
+        $this->$type[$name] = array();
+      }
+      else {
+        $name = $i;
+      }
+      $def =& $this->{$type}[$name];
+      
+      if (empty($def['table'])) {
+        $def['table'] = String::pluralize(String::underscore($name));
+      }
+      if (empty($def['primary_key'])) {
+        $def['primary_key'] = 'id';
+      }
+      if (empty($def['foreign_key']))
+      {
+        switch($type)
+        {
+          case 'belongs_to':
+            $def['foreign_key'] = String::underscore($name).'_'.$def['primary_key'];
+          break;
+          
+          case 'has_one':
+            $def['foreign_key'] = String::underscore(get_class($this)).'_'.$this->primary_key;
+          break;
+          
+          case 'has_many':
+            $def['foreign_key'] = String::underscore(get_class($this)).'_'.$this->primary_key;
+          break;
+        }
+      }
     }
   }
   
@@ -83,39 +135,35 @@ class ActiveRecord_Base extends ActiveRecord_Record
   
   function __get($attribute)
   {
-    if (in_array($attribute, array_keys($this->columns)))
-    {
-      # field
+    # field
+    if (in_array($attribute, array_keys($this->columns))) {
       return parent::__get($attribute);
     }
-    elseif (in_array($attribute, $this->belongs_to))
+    
+    # association: belongs to
+    elseif (array_key_exists($attribute, $this->belongs_to))
     {
-      # association: belongs to
-      $foreign_key = "{$attribute}_id";
-      $conditions  = array('id' => $this->{$foreign_key});
-      $class       = String::camelize($attribute);
-      
-      $record = new $class();
+      $conditions = array($this->belongs_to[$attribute]['primary_key'] => $this->{$this->belongs_to[$attribute]['foreign_key']});
+      $class      = String::camelize($attribute);
+      $record     = new $class();
       return $this->$attribute = $record->find(':first', array('conditions' => &$conditions));
     }
-    elseif (in_array($attribute, $this->has_one))
+    
+    # association: has one
+    elseif (array_key_exists($attribute, $this->has_one))
     {
-      # association: has one
-      $foreign_key = String::underscore(get_class($this))."_id";
-      $conditions  = array($foreign_key => $this->{$this->primary_key});
-      $class       = String::camelize($attribute);
-      
-      $record = new $class();
+      $conditions = array($this->has_one[$attribute]['foreign_key'] => $this->{$this->primary_key});
+      $class      = String::camelize($attribute);
+      $record     = new $class();
       return $this->$attribute = $record->find(':first', array('conditions' => &$conditions));
     }
-    elseif (in_array($attribute, $this->has_many))
+    
+    # association: has many
+    elseif (array_key_exists($attribute, $this->has_many))
     {
-      # association: has many
-      $foreign_key = String::underscore(get_class($this))."_id";;
-      $conditions  = array($foreign_key => $this->{$this->primary_key});
-      $class       = String::camelize(String::singularize($attribute));
-      
-      $record = new $class();
+      $conditions = array($this->has_many[$attribute]['foreign_key'] => $this->{$this->primary_key});
+      $class      = String::camelize(String::singularize($attribute));
+      $record     = new $class();
       return $this->$attribute = $record->find(':all', array('conditions' => &$conditions));
     }
   }
