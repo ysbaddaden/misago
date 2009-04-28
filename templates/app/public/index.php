a57ea098f263963fdeb6b40572d47431b9e78caa
Julien <ysbaddaden@gmail.com>
2009-04-28 23:47:01 +0200

    Fixed the non-processing of URL query string ($_GET) when using the 404 handler to serve requests.

--- templates/app/public/index.php
+++ templates/app/public/index.php
@@ -7,10 +7,18 @@ require dirname(__FILE__).'/../config/boot.php';
 
 try
 {
-  ActionController_dispatch(
-    isset($_POST['_method']) ? strtoupper($_POST['_method']) : $_SERVER['REQUEST_METHOD'],
-    $_SERVER['REQUEST_URI']
-  );
+  $method = isset($_POST['_method']) ? strtoupper($_POST['_method']) : $_SERVER['REQUEST_METHOD'];
+  $uri    = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+  
+  # using the 404-handler QUERY_STRING is undefined, and $_GET isn't populated
+  if (empty($_SERVER['QUERY_STRING']))
+  {
+    $_SERVER['QUERY_STRING'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+    parse_str($_SERVER['QUERY_STRING'], $_GET);
+  }
+  $_REQUEST = array_merge($_GET, $_POST);
+  
+  ActionController_dispatch($method, $uri);
 }
 catch(MisagoException $e) {
   $e->render();
