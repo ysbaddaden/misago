f9f75dd6a023c0aa82c9f2ffe6e8cabf7838be28
Julien <ysbaddaden@gmail.com>
2009-07-03 18:59:55 +0200

    ActiveRecord_Base now longer overwrites already specified timestamps (create_at, created_on, updated_at & updated_on).

--- lib/active_record/base.php
+++ lib/active_record/base.php
@@ -548,10 +548,10 @@ abstract class ActiveRecord_Base extends ActiveRecord_Validations
     }
     
     # timestamps
-    if (array_key_exists('created_at', $this->columns)) {
+    if (array_key_exists('created_at', $this->columns) and empty($this->created_at)) {
       $this->created_at = new Time(null, 'datetime');
     }
-    if (array_key_exists('created_on', $this->columns)) {
+    if (array_key_exists('created_on', $this->columns) and empty($this->created_on)) {
       $this->created_on = new Time(null, 'date');
     }
     
@@ -593,10 +593,10 @@ abstract class ActiveRecord_Base extends ActiveRecord_Validations
     }
     
     # timestamps
-    if (array_key_exists('updated_at', $this->columns)) {
+    if (array_key_exists('updated_at', $this->columns) and empty($this->updated_at)) {
       $this->updated_at = new Time(null, 'datetime');
     }
-    if (array_key_exists('updated_on', $this->columns)) {
+    if (array_key_exists('updated_on', $this->columns) and empty($this->updated_on)) {
       $this->updated_on = new Time(null, 'date');
     }
     
