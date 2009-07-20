dd983a0b552ca2d77e9524a02ce683a944f0dea6
Julien <ysbaddaden@gmail.com>
2009-07-20 15:19:56 +0200

    Added current_page and link_to_unless_current to ActionView_Helpers_UrlHelper.

--- lib/action_view/helpers/url_helper.php
+++ lib/action_view/helpers/url_helper.php
@@ -1,4 +1,40 @@
 <?php
+
+# Checks wether the given URL is the current page or not.
+# 
+# Let's say we are visiting `/products?order=asc`:
+# 
+#   current_page('/products')
+#   # => true
+# 
+#   current_page(array(':controller' => '/products'))
+#   # => true
+# 
+#   current_page(array(':controller' => '/products', 'order' => 'desc'))
+#   # => false
+# 
+#   current_page(array(':controller' => '/products', 'order' => 'asc'))
+#   # => true
+# 
+#   current_page(array(':controller' => '/products', ':action' => 'show'))
+#   # => false
+# 
+# @namespace ActionView_Helpers_UrlHelper
+function current_page($url)
+{
+  if (!is_string($url)) {
+    $url = (string)path_for($url);
+  }
+  
+  # URL has no query string? let's compare the path only:
+  if (strpos($url, '?') === false)
+  {
+    $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    return ($url == $request_uri);
+  }
+  return ($url == $_SERVER['REQUEST_URI']);
+}
+
 # Creates a link.
 # 
 # @namespace ActionView_Helpers_UrlHelper
@@ -21,27 +57,16 @@ function link_to($content, $url, $attributes=null)
   return tag('a', $content, $attributes);
 }
 
-# Returns the URL for a given mapping.
+# Renders a link unless it points to the current page. Otherwise
+# displays the content into a SPAN tag with no attributes.
 # 
 # @namespace ActionView_Helpers_UrlHelper
-function url_for($mapping)
+function link_to_unless_current($content, $url, $attributes=null)
 {
-  $map = ActionController_Routing::draw();
-  return $map->reverse($mapping);
-}
-
-# TODO: mail_to()
-# @namespace ActionView_Helpers_UrlHelper
-function mail_to()
-{
-  
-}
-
-# TODO: is_current_page()
-# @namespace ActionView_Helpers_UrlHelper
-function is_current_page()
-{
-  
+  if (current_page($url)) {
+    return tag('span', $content);
+  }
+  return link_to($content, $url, $attributes);
 }
 
 # TODO: button_to()
@@ -51,11 +76,12 @@ function button_to()
   
 }
 
-# TODO: link_to_unless_current()
+# TODO: mail_to()
 # @namespace ActionView_Helpers_UrlHelper
-function link_to_unless_current()
+function mail_to()
 {
   
 }
+
 
 ?>
--- test/unit/action_view/helpers/test_url_helper.php
+++ test/unit/action_view/helpers/test_url_helper.php
@@ -22,6 +22,30 @@ class Test_ActionView_Helpers_UrlHelper extends Unit_Test
     $html_link = link_to('destroy me', new ActionController_Path('DELETE', 'page/123'), array('class' => 'destroy'));
     $this->assert_equal("", $html_link, '<a class="destroy request_method:delete" href="/page/123">destroy me</a>');
   }
+  
+  function test_current_page()
+  {
+    $_SERVER['REQUEST_URI'] = '/archives';
+    $this->assert_true('',  current_page('/archives'));
+    $this->assert_false('', current_page('/articles'));
+    $this->assert_true('',  current_page(array(':controller' => 'archives')));
+    $this->assert_false('', current_page(array(':controller' => 'accounts')));
+    $this->assert_false('', current_page(array(':controller' => 'archives', ':action' => 'categories')));
+    
+    $_SERVER['REQUEST_URI'] = '/articles/show/45';
+    $this->assert_false('with params: failure', current_page(array(':controller' => 'archives', ':action' => 'show', ':id' => 45)));
+    $this->assert_true('with params: success',  current_page(array(':controller' => 'articles', ':action' => 'show', ':id' => 45)));
+    
+    $_SERVER['REQUEST_URI'] = '/articles?order=desc';
+    $this->assert_true('true even without GET params', current_page(array(':controller' => 'articles')));
+    $this->assert_false('wrong GET param', current_page(array(':controller' => 'articles', 'order' => 'asc')));
+    $this->assert_true('good GET params',  current_page(array(':controller' => 'articles', 'order' => 'desc')));
+  }
+  
+  function test_link_to_unless_current()
+  {
+    
+  }
 }
 
 new Test_ActionView_Helpers_UrlHelper();
