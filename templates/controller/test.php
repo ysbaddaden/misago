9a242e2568c0435c5ae4fa02e4e8489464bcd495
Julien <ysbaddaden@gmail.com>
2009-08-20 18:50:03 +0200

    Fixed a bug in controller test template (for script/generate controller xxx).

--- templates/controller/test.php
+++ templates/controller/test.php
@@ -3,8 +3,7 @@
 if (!isset($_SERVER['MISAGO_ENV'])) {
   $_SERVER['MISAGO_ENV'] = 'test';
 }
-$location = dirname(__FILE__).'/../..';
-require_once "$location/app/controllers/#{filename}_controller.php";
+require_once dirname(__FILE__).'/../../config/boot.php';
 
 class Test_#{Class}Controller extends Unit_TestCase
 {
