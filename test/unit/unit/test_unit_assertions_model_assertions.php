cd3da0cab79324c16483d72cdc7ba6962a20f291
ysbaddaden <ysbaddaden@gmail.com>
2009-09-23 17:00:37 +0200

    Added ModelAssertions::is_valid().

---
+++ test/unit/unit/test_unit_assertions_model_assertions.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!isset($_SERVER['MISAGO_ENV'])) {
+  $_SERVER['MISAGO_ENV'] = 'test';
+}
+require_once dirname(__FILE__)."/../../test_app/config/boot.php";
+
+class Test_Unit_Assertions_ModelAssertions extends Unit_TestCase
+{
+  function test_assert_valid()
+  {
+    $product = new Product(array('name' => 'keyboard', 'price' => 6.0));
+    $this->assert_valid('', $product);
+  }
+}
+
+new Test_Unit_Assertions_ModelAssertions();
+
+?>
