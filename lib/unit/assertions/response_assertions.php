ce1594b32902add04c02427d57f91a33619aa5ce
Julien <ysbaddaden@gmail.com>
2009-09-29 10:11:55 +0200

    Fixed ResponseAssertions.

--- lib/unit/assertions/response_assertions.php
+++ lib/unit/assertions/response_assertions.php
@@ -4,40 +4,40 @@ class Unit_Assertions_ResponseAssertions extends Unit_Assertions_DomAssertions
 {
   protected $response = array();
   
-  protected function assert_redirected_to($comment, $url)
+  protected function assert_redirected_to($url, $comment=null)
   {
     $location = isset($this->response['headers']['location']) ?
       $this->response['headers']['location'] : false;
-    $this->assert_equal($comment, $location, ($url === false) ? false : (string)$url);
+    $this->assert_equal($location, ($url === false) ? false : (string)$url, $comment);
   }
   
-  protected function assert_response($comment, $status)
+  protected function assert_response($status, $comment=null)
   {
-    $this->assert_equal($comment, $this->response['status'], $status);
+    $this->assert_equal($this->response['status'], $status, $comment);
   }
   
-  protected function assert_cookie_presence($comment, $cookie)
+  protected function assert_cookie_presence($cookie, $comment=null)
   {
-    $this->assert_true($comment, isset($this->response['headers']['cookies'][$cookie]));
+    $this->assert_true(isset($this->response['headers']['cookies'][$cookie]), $comment);
   }
   
-  protected function assert_cookie_not_present($comment, $cookie)
+  protected function assert_cookie_not_present($cookie, $comment=null)
   {
-    $this->assert_false($comment, isset($this->response['headers']['cookies'][$cookie]));
+    $this->assert_false(isset($this->response['headers']['cookies'][$cookie]), $comment);
   }
   
-  protected function assert_cookie_equal($comment, $cookie, $expected)
-  {
-    $value = isset($this->response['headers']['cookies'][$cookie]) ?
-      $this->response['headers']['cookies'][$cookie] : null;
-    $this->assert_equal($comment, $value, $expected);
-  }
-  
-  protected function assert_cookie_not_equal($comment, $cookie, $expected)
+  protected function assert_cookie_equal($cookie, $expected, $comment=null)
   {
     $value = isset($this->response['headers']['cookies'][$cookie]) ?
       $this->response['headers']['cookies'][$cookie] : null;
-    $this->assert_not_equal($comment, $value, $expected);
+    $this->assert_equal($value, $expected, $comment);
+  }
+  
+  protected function assert_cookie_not_equal($cookie, $expected, $comment=null)
+  {
+    $value = isset($this->response['headers']['cookies'][$cookie]) ?
+      $this->response['headers']['cookies'][$cookie] : null;
+    $this->assert_not_equal($value, $expected, $comment);
   }
   
   protected function test_assert_template()
