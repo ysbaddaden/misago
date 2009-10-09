fa6ddb8c737cf40779988072fc2a8a44957e1e8d
ysbaddaden <ysbaddaden@gmail.com>
2009-10-09 10:58:38 +0200

    Didn't tested before commiting. That's bad, since Yaml was broken

--- lib/yaml.php
+++ lib/yaml.php
@@ -2,13 +2,13 @@
 
 class Yaml
 {
-  static function decode($yaml)
+  static function decode($input)
   {
     $spyc = new Spyc();
     return $spyc->load($input);
   }
   
-  static function encode($string)
+  static function encode($array)
   {
     $spyc = new Spyc();
     return $spyc->dump($array, false, false);
