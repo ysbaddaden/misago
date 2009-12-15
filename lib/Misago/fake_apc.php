affa84a29e330a61c679abf3c19f54dc8566feb7
Julien <ysbaddaden@gmail.com>
2009-12-15 15:46:06 +0100

    Fixed fake APC storage with multiple keys.

--- lib/Misago/fake_apc.php
+++ lib/Misago/fake_apc.php
@@ -6,7 +6,6 @@ $GLOBALS['__fake_apc_data'] = array();
 
 # Caches a variable in the data store, only if it's not already stored. 
 # 
-# FIXME: handle array of keys.
 # :nodoc:
 function apc_add($key, $var, $ttl=0)
 {
@@ -20,9 +19,17 @@ function apc_add($key, $var, $ttl=0)
 # 
 # FIXME: handle array of keys.
 # :nodoc:
-function apc_store($key, $var, $ttl=0)
+function apc_store($key, $var=null, $ttl=0)
 {
-  $GLOBALS['__fake_apc_data'][$key] = $var;
+  if (is_array($key))
+  {
+    foreach($key as $k => $v) {
+      $GLOBALS['__fake_apc_data'][$k] = $v;
+    }
+  }
+  else {
+    $GLOBALS['__fake_apc_data'][$key] = $var;
+  }
   return true;
 }
 
@@ -32,7 +39,19 @@ function apc_store($key, $var, $ttl=0)
 # :nodoc:
 function apc_fetch($key, &$success=null)
 {
-  if (isset($GLOBALS['__fake_apc_data'][$key]))
+  if (is_array($key))
+  {
+    $rs     = array();
+    foreach($key as $k)
+    {
+      if (isset($GLOBALS['__fake_apc_data'][$k])) {
+        $rs[$k] = $GLOBALS['__fake_apc_data'][$k];
+      }
+    }
+    $sucess = true;
+    return $rs;
+  }
+  elseif (isset($GLOBALS['__fake_apc_data'][$key]))
   {
     $success = true;
     return $GLOBALS['__fake_apc_data'][$key];
@@ -45,7 +64,15 @@ function apc_fetch($key, &$success=null)
 # :nodoc:
 function apc_delete($key)
 {
-  unset($GLOBALS['__fake_apc_data'][$key]);
+  if (is_array($key))
+  {
+    foreach($key as $k) {
+      unset($GLOBALS['__fake_apc_data'][$k]);
+    }
+  }
+  else {
+    unset($GLOBALS['__fake_apc_data'][$key]);
+  }
   return true;
 }
 
