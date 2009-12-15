f9a9918a9620f9aa9992c1b4808224158fe2ca03
Julien <ysbaddaden@gmail.com>
2009-12-15 16:24:02 +0100

    I18n translations may now be stored in multiple files in config/locales//.

--- lib/Misago/I18n.php
+++ lib/Misago/I18n.php
@@ -4,18 +4,53 @@ require 'I18n_shortcuts.php';
 
 # Handles translation of strings.
 # 
-# =Translating a string
+# =Syntax
 # 
-#   I18n::translate('my string')
-#   t('my other string')
+# Translating a string is just a key/value pairing system. If you want to
+# translate 'this is my name' in french, all you need a YAML file like this:
+# 
+#   fr:
+#     "this is my name": "ceci est mon nom"
+# 
+# And then to translate it, call <tt>Misago\I18n::translate</tt> or it's
+# shorcut '+t()+:
+# 
+#   $translated_string = t('this is my name');
+#   # => "ceci est mon nom"
+# 
+# ==Files
+# 
+# Translations are stored in a YAML file under +config/locales/+. For instance
+# english translations are in +config/locales/en.yml+, while french ones will be
+# in +config/locales/fr.yml+, etc.
+# 
+# You may also separate translations in multiple YAML file, by storing them
+# under +config/locales/$locale/+, so you may have:
+# 
+#   config/locales/en.yml
+#   config/locales/en/posts/index.yml
+#   config/locales/en/posts/show.yml
+#   config/locales/en/posts/new.yml
+#   config/locales/en/posts/edit.yml
 # 
 # =Context
 # 
-# You may specify a particular context to search the translation in.
-# Either as passing the 'context' option, or by prepending the
-# string in the following way: +context.string+
+# You may specify a particular context to search the translation in. Either as
+# passing the 'context' option, or by prepending the string in the following
+# way: +context.string+
 # 
-# You may have subcontexts, like: +active_record.error.messages.empty+
+# You may have subcontexts, like: +active_record.error.messages+
+# 
+# This is reflected in the YAML file as hashes:
+# 
+#   en:
+#     active_record:
+#       error:
+#         messages:
+#           empty: "{{attribute} can't be empty"
+# 
+#   t('empty', array('context' => 'active_record.error.messages',
+#     'attribute' => 'name'))
 # 
 # =Interpolation
 # 
@@ -27,16 +62,16 @@ require 'I18n_shortcuts.php';
 #   t('{{user_name}} sent you a message', array('user_name' => 'James'));
 #   # => James sent you a message
 # 
-# =Pluralization [todo, specification unfinished]
-# 
-# There must multiple translations in your YAML file for a single message.
-# In fact as many translations as there are possible results to the above
-# computing. Eventually, you need to pass a +count+ interpolation variable,
-# that will determine which translation to use.
+# =Pluralization
 # 
 # Depending on your locale, a computing is done to determine which translation
 # must be selected based on the +count+ variable. The algorythm is the same
 # than the one used by +gettext+.
+# 
+# There must be multiple translations in your YAML file for a single message.
+# In fact as many translations as there are possible results to the above
+# computing. Eventually, you need to pass a +count+ interpolation variable,
+# that will determine which translation to use.
 # 
 # Examples:
 # 
@@ -50,11 +85,11 @@ require 'I18n_shortcuts.php';
 #   
 #   t('x_minutes', array('count' => 1));   # => 'a minute'
 #   t('x_minutes', array('count' => 12));  # => '12 minutes'
+#   
 #   t('there_are_x_messages', array('count' => 0));   # => There are 0 messages
 #   t('there_are_x_messages', array('count' => 1));   # => There is 1 message
 #   t('there_are_x_messages', array('count' => 29));  # => There are 29 messages
 # 
-# TODO: Separate translations in a directory structure (eg: use context as subpath).
 class I18n
 {
   static public  $locale       = 'en';
@@ -167,14 +202,24 @@ class I18n
       # misago's translations
       $contents     = file_get_contents(MISAGO."/lib/Misago/locales/$locale.yml");
       $translations = yaml_decode($contents);
-
+      
       # app's translations
       $locale_file = ROOT."/config/locales/$locale.yml";
-      if (file_exists($locale_file))
+      if (file_exists($locale_file)) {
+        self::load_translations_from_file($locale_file, $translations);
+      }
+      
+      $locale_path = ROOT."/config/locales/$locale";
+      if (file_exists($locale_path) and is_dir($locale_path))
       {
-        $contents      = file_get_contents($locale_file);
-        $_translations = yaml_decode($contents);
-        $translations  = hash_merge_recursive($translations, $_translations);
+        $d = opendir($locale_path);
+        while(($f = readdir($d)) !== false)
+        {
+          if (strpos($f, '.yml') === strlen($f)-4) {
+            self::load_translations_from_file("$locale_path/$f", $translations);
+          }
+        }
+        closedir($d);
       }
       
       if (!empty($translations[$locale])) {
@@ -184,6 +229,13 @@ class I18n
     }
     
     self::$translations[$locale] = $translations;
+  }
+  
+  static private function load_translations_from_file($file, &$translations)
+  {
+    $contents      = file_get_contents($file);
+    $_translations = yaml_decode($contents);
+    $translations  = hash_merge_recursive($translations, $_translations);
   }
   
   static private function & flatten_translation_keys($ary, $parent='')
